71ec7073959709002445d50cfc99bfed22bbadee
MarkBoekhoud <markboekhoud@outlook.com>
2025-12-17 14:58:10 +0100

    New Sync

--- supabase/migrations/20251216171101_f9ac1bef-5dd4-4caf-a320-d74af4bfef87.sql
+++ supabase/migrations/20251216171101_f9ac1bef-5dd4-4caf-a320-d74af4bfef87.sql
@@ -4,5 +4,5 @@ ADD COLUMN speed character varying DEFAULT NULL,
 ADD COLUMN visibility character varying DEFAULT NULL,
 ADD COLUMN bpm integer DEFAULT NULL,
 ADD COLUMN spm integer DEFAULT NULL,
-ADD COLUMN excluded_ghosts TEXT[] DEFAULT ARRAY[]::TEXT[],
-ADD COLUMN selected_abilities TEXT[] DEFAULT ARRAY[]::TEXT[];
+ADD COLUMN excluded_ghosts integer DEFAULT NULL,
+ADD COLUMN selected_abilities integer DEFAULT NULL,
