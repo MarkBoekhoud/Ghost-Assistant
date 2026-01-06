6d1d8dcfe6d4d329ca102a8c83345f32eee5e0be
MarkBoekhoud <markboekhoud@outlook.com>
2026-01-06 16:54:01 +0100

    Build

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,12 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: "/Ghost-Assistant/",
+  
+  // DIT STUKJE HEB IK TOEGEVOEGD:
+  build: {
+    outDir: "docs",
+  },
+
   server: {
     host: "::",
     port: 8080,
